b32d13ce6c392195b456060c8d9f731235036c5e
Aaron Coburn <acoburn@amherst.edu>
2014-02-10 10:58:40 -0500

    removed  debugging

--- lib/omniauth/strategies/five_colleges.rb
+++ lib/omniauth/strategies/five_colleges.rb
@@ -1,9 +1,5 @@
 require 'omniauth-oauth'
 require 'multi_json'
-require 'logger'
-
-logger = Logger.new(STDOUT)
-logger.level = Logger::WARN
 
 module OmniAuth
   module Strategies
@@ -24,10 +20,9 @@ module OmniAuth
       
       info do 
         {
-          :email => user_info['emails'][0],
+          :email => user_info['email'],
           :name => user_info['displayName'],
-          :id => user_info['id'],
-          :provider => "fivecolleges"
+          :id => user_info['id']
         }
       end
       
@@ -40,7 +35,6 @@ module OmniAuth
       # Return info gathered from the flickr.people.getInfo API call 
      
       def raw_info
-        logger.warn("Getting user profile")
         @raw_info ||= MultiJson.decode(access_token.get('/oauth/user_profile').body)
       rescue ::Errno::ETIMEDOUT
         raise ::Timeout::Error
@@ -49,8 +43,6 @@ module OmniAuth
       # Provide the "Person" portion of the raw_info
       
       def user_info
-        logger.warn('Getting raw info')
-        logger.warn(raw_info)
         @user_info ||= raw_info.nil? ? {} : raw_info
       end
       
